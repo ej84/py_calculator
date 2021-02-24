ff504c7c564d170d0a972e0e19ef341373eb3ee8
ej84 <ej84@njit.edu>
2021-02-24 11:38:29 -0500

    task: modified test_Calculator

--- tests/test_Calculator.py
+++ tests/test_Calculator.py
@@ -4,11 +4,13 @@ from src.calculator.calculator import Calculator
 
 
 class CalculatorTestCase(unittest.TestCase):
+
     def setUp(self) -> None:
         self.calculator = Calculator()
 
     def test_instantiate_calculator(self):
-        self.assertIsInstance(self.calculator, Calculator)
+        calculator1 = Calculator()
+        self.assertIsInstance(calculator1, Calculator)
 
     def test_result_is_zero_calculator(self):
         self.assertEqual(self.calculator.result, 0)
