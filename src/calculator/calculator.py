709d49de76bc03fcf2ff6ae47abc22bd5222f99c
ej84 <ej84@njit.edu>
2021-02-24 19:41:04 -0500

    task: Created addition.py

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -3,3 +3,11 @@ class Calculator:
 
     def __init__(self):
         pass
+
+    def add(self, a , b):
+        self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
+        return self.result
